76558df2ebe2ddab38aee540bccd0a1cc2e2b04d
Taregnaeem <tareghafeez@gmail.com>
2022-09-20 00:41:17 +0300

    Maven Project commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+@Test
+  public void greetShouldIncludeGreeting() {
+    String someone = "World, Modification fo the Jenkins file";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+
 }
